449630aede0a392e386ed6d5f0b91a002c400321
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-09-14 23:05:43 -0300

    .

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function imgSlider(e){
+    document.getElementById('phone').src = e;
+}
+function circle(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
